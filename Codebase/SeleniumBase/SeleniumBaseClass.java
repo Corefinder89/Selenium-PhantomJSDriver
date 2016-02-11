76d774e952e300eca8e2f41f89f35c0fc8594609
SoumyajitBasu1988 <soumyajit.basu@srijan.in>
2016-02-11 17:38:09 +0530

    Added commenting of code in the login.java file for better readability of code structure

--- Codebase/SeleniumBase/SeleniumBaseClass.java
+++ Codebase/SeleniumBase/SeleniumBaseClass.java
@@ -8,10 +8,10 @@ import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class SeleniumBaseClass<driver>
+public class SeleniumBaseClass
 {
 	FileReader instream;
-	public String getDataFromConfigFile(String variable)
+	public String getURLFromConfigFile(String variable)
 	{
 		String value="";
 		try
@@ -20,7 +20,7 @@ public class SeleniumBaseClass<driver>
 			 * Get the present user working directory
 			 */
 			final String dir = System.getProperty("user.dir");
-	        System.out.println("Current directory is = " + dir);
+	        System.out.println("Current directory for URL is = " + dir);
 	        /*
 	         * Absolute path for config.properties file
 	         */
@@ -56,7 +56,7 @@ public class SeleniumBaseClass<driver>
 		return value;
 	}
 	
-	public String getDatafromConfigFile(String variable)
+	public String getUsernamefromConfigFile(String variable)
 	{
 		String value="";
 		try
@@ -65,7 +65,52 @@ public class SeleniumBaseClass<driver>
 			 * Get the present user working directory
 			 */
 			final String dir = System.getProperty("user.dir");
-	        System.out.println("Current directory is = " + dir);
+	        System.out.println("Current directory for username is = " + dir);
+	        /*
+	         * Absolute path for config.properties file
+	         */
+			String path="/Users/soumyajit/Documents/HTMLUnit/WELLKEPT/Codebase/Configuration/Config.properties";
+			/*
+			 * instream is an instance of FileReader and will read the contents of the config.properties file
+			 */
+			instream = new FileReader(path);
+			Properties prop = new Properties();
+			/*
+			 * Load the contents of the file read using the load method of the properties class
+			 */
+			prop.load(instream);
+			/*
+			 * The variable value will store the value of the variable that has been passed as
+			 * an argument
+			 */
+			value = prop.getProperty(variable);
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println("File Not found");
+			e.printStackTrace();
+		}
+		catch (IOException e) 
+		{
+			System.out.println("I/O Exception");
+			e.printStackTrace();
+		}
+		/*
+		 * Return the value of the corresponding variable to the calling program
+		 */
+		return value;
+	}
+	
+	public String getPasswordfromConfigFile(String variable)
+	{
+		String value="";
+		try
+		{
+			/*
+			 * Get the present user working directory
+			 */
+			final String dir = System.getProperty("user.dir");
+	        System.out.println("Current directory for password is = " + dir);
 	        /*
 	         * Absolute path for config.properties file
 	         */
@@ -103,14 +148,28 @@ public class SeleniumBaseClass<driver>
 	
 	public Capabilities CheckDriverConfigurations()
 	{
-		// prepare capabilities
+		/*Desired capabilities is used to configure the driver instance of webdriver. All driver instances can be configured using
+		 *desired capability class	
+		 */
 	    Capabilities caps = new DesiredCapabilities();
-	    ((DesiredCapabilities) caps).setJavascriptEnabled(true);                
+	    /*
+	     * Enable javascript for browser
+	     */
+	    ((DesiredCapabilities) caps).setJavascriptEnabled(true);
+	    /*
+	     * Set capability to take screenshot
+	     */
 		 ((DesiredCapabilities) caps).setCapability("takesScreenshot", true);
+		 /*
+		  * Set capability for the executable path for phantomjs
+		  */
 		 ((DesiredCapabilities) caps).setCapability(
 		        PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
 		        "//Users//soumyajit//Documents//Zippers//phantomjs//bin//phantomjs"
 		    );
-       return caps;	
+		 /*
+		  * return capability instance to increase reusability of capability
+		  */
+		 return caps;	
 	}
 }
